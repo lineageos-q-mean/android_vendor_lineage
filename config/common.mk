aabc46da476f8463b2c793eaf3843b071093aa92
MSe1969 <mse1969@posteo.de>
2024-03-07 21:31:04 +0100

    config: Add new property to indicate custom build version [1/3]
    AOSP has stopped providing patches for Android 10.
    Consequently, LineageOS will not lift the patch level any longer, see comment
    here: https://review.lineageos.org/c/LineageOS/android_build/+/320334
    
    To consider that (not lifting the patch), but at the same time indicate,
    that despite the missing patches, further backporting of security issues
    is ongoing, a new "Custom build release" property 'ro.lineage.custom_version'
    is introduced to show this next to the "official" security string.
    
    Change-Id: Ie3d8c6a68fb88283420adbb6ecc72613d8028d5e
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- config/common.mk
+++ config/common.mk
@@ -1,3 +1,6 @@
+# Unofficial patch level
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.lineage.custom_version=2024-10-01
+
 # Allow vendor/extra to override any property by setting it first
 $(call inherit-product-if-exists, vendor/extra/product.mk)
 
