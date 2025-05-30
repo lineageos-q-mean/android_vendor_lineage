WingsOSab/android_vendor_wings

vendor: vendorsetup: Override host metadata
Change-Id: Ie7f64dfd2194798acc151b7826d1c3da80eb0005
Signed-off-by: S I P U N <sipunkumar85@gmail.com>

vendorsetup.sh
@@ -1,5 +1,9 @@
 # Shebang is intentionally missing - do not run as a script
 
+# Override host metadata to make builds more reproducible and avoid leaking info
+export BUILD_USERNAME=yui
+export BUILD_HOSTNAME=everest-buildbot
+
 # ABI compatibility checks fail for several reasons:
 #   - The update to Clang 12 causes some changes, but no breakage has been
 #     observed in practice.

lineageos-q-mean/android_vendor_lineage

Signed-off-by: texascake <tiktodz@soulvibe-team.cloud>

vendorsetup.sh
@@ -1,5 +1,9 @@
 # Shebang is intentionally missing - do not run as a script
 
+# Override host metadata to make builds more reproducible and avoid leaking info
+export BUILD_USERNAME=nobody
+export BUILD_HOSTNAME=android-build
+
 # ABI compatibility checks fail for several reasons:
 #   - The update to Clang 12 causes some changes, but no breakage has been
 #     observed in practice.