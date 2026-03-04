9af445d6acdfafee53a3018cbaa32dfa27e890b3
Tiktodz <dotkit@electrowizard.me>
2026-03-04 21:58:44 +0000

    Bump Custom build version 2025-12-01
    https://review.lineageos.org/q/topic:Q_asb_2025-12
    
    Change-Id: Ifa2b2c108fa22e341413dba3e0aead3c2d3f6815

--- config/common.mk
+++ config/common.mk
@@ -1,5 +1,5 @@
 # Unofficial patch level
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.lineage.custom_version=2025-05-01
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.lineage.custom_version=2025-12-01
 
 # Allow vendor/extra to override any property by setting it first
 $(call inherit-product-if-exists, vendor/extra/product.mk)
